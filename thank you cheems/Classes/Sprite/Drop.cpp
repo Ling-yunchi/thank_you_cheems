114d0e465110c3f6e00e6c158c77f3fa0b11b512
Ling_yunchi <1005175850@qq.com>
2021-07-16 11:23:56 +0800

    修改gamescene

---
+++ thank you cheems/Classes/Sprite/Drop.cpp
@@ -0,0 +1,16 @@
+//#include "Drop.h"
+//
+//Drop* Drop::create()
+//{
+//	drop_ = Sprite::create("drop.png");
+//	if (drop_ && init()) {
+//		
+//	}
+//}
+//
+//bool Drop::init()
+//{
+//	auto body = cocos2d::PhysicsBody::createCircle(100);
+//	
+//}
+//
---
+++ thank you cheems/Classes/Sprite/Drop.h
@@ -0,0 +1,12 @@
+//#pragma once
+//#include <cocos2d.h>
+//
+//class Drop: public cocos2d::Sprite
+//{
+//public:
+//	Drop* create();
+//private:
+//	Sprite* drop_;
+//	bool init() override;
+//};
+//
