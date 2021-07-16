0e3b57de1ecb500b1e3c97d7fa95f14570138a32
梅 零 落 <1187034639@qq.com>
2021-07-16 14:32:06 +0800

    cheems

--- thank you cheems/Classes/Scene/GameScene/GameScene.cpp
+++ thank you cheems/Classes/Scene/GameScene/GameScene.cpp
@@ -38,6 +38,9 @@ void GameScene::createSprites()
 	auto pos = map_->getObjectGroup("sprites")->getObject("cheems");
 	cheems_->setPosition(pos["x"].asFloat(),pos["y"].asFloat());
 	cheems_->setScale(0.1);
+	cheems_->getPhysicsBody()->setCategoryBitmask(0x1);
+	cheems_->getPhysicsBody()->setCollisionBitmask(0x1);
+	cheems_->getPhysicsBody()->setContactTestBitmask(0x2);
 	map_->addChild(cheems_, 999);
 
 	factory->loadDragonBonesData("Animation/Sweating_soybean/Sweating_soybean_ske.json", "soybean");
@@ -46,6 +49,10 @@ void GameScene::createSprites()
 	auto soybean = AnimationSprite::create("soybean", Size(300, 195));
 	soybean->setScale(0.2);
 	soybean->setPosition(VisibleRect::center());
+	soybean->getPhysicsBody()->setCategoryBitmask(0x2);
+	soybean->getPhysicsBody()->setCollisionBitmask(0x1);
+	soybean->getPhysicsBody()->setContactTestBitmask(0x1);
+
 	map_->addChild(soybean, 999);
 }
 
